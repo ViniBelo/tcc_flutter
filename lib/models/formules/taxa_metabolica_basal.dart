b096722c3d8da940c7ffabdac8ec1a2f8d099131
ViniBelo <vinialvesbelo@outlook.com>
2023-08-14 23:20:02 -0300

    Pequena alteração

--- lib/models/formules/taxa_metabolica_basal.dart
+++ lib/models/formules/taxa_metabolica_basal.dart
@@ -1,9 +1,9 @@
-import 'package:estudos_flutter/models/enums/sexo.dart';
-
+// ignore: non_constant_identifier_names
 double homem_tmb(double peso, double altura, int idade) {
   return 66.0 + (13.7 * peso) + (5.0 * altura) + (6.8 * idade);
 }
 
+// ignore: non_constant_identifier_names
 double mulher_tmb(double peso, double altura, int idade) {
   return 65.5 + (9.6 * peso) + (1.8 * altura) + (4.7 * idade);
 }
